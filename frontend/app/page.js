1b0ed5ae394d7b252597dc2bc41788cdbd939c30
kushi947 <chaminikushani2@gmail.com>
2026-04-18 13:19:10 +0530

    merge conflict

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Hello</h1>
-      </main>
+    <div className="flex items-center justify-center min-h-screen">
+      <Link href="/traffic-game" className="p-4 bg-blue-600 text-white rounded">
+        Open Simulation UI
+      </Link>
     </div>
   );
 }
