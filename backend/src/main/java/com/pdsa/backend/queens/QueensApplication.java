28931363c18717fb68ebedd8ce7aa59d3109e412
Yasirujm <jinadith23@gmail.com>
2026-04-21 14:28:56 +0530

    fixes

--- backend/src/main/java/com/pdsa/backend/queens/QueensApplication.java
+++ backend/src/main/java/com/pdsa/backend/queens/QueensApplication.java
@@ -1,13 +1,13 @@
-package com.pdsa.backend.queens;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class QueensApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(QueensApplication.class, args);
-	}
-
-}
+//package com.pdsa.backend.queens;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class QueensApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(QueensApplication.class, args);
+//	}
+//
+//}
